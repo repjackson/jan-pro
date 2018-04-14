5ae5f8478913a8d339d858162052f9b36afd8276
repjackson <repjackson@gmail.com>
2018-04-14 06:44:40 +0000

    login page interface update, nonworking logic

--- template/imports/ui/pages/Auth/Login.vue
+++ template/imports/ui/pages/Auth/Login.vue
@@ -1,103 +1,91 @@
 <template lang='pug'>
-  .login
-    .auth-layout_inner
-      q-card.auth-layout_card.card
-        header.auth-layout_card-header.card-header
-          img.logo(src="/images/logo.svg" width="60" height="60" alt="Logo")
-          h2 Login
-        span.divider
-        <!--el-form.auth-layout_card-content.card-content(:model="loginForm" :rules="rules" ref="loginForm")-->
-        <!--  el-form-item(prop="email" label="Email")-->
-        <!--    el-input(v-model="loginForm.email" v-on:keyup.enter="submitForm('loginForm')" auto-complete="off")-->
-        <!--  el-form-item(prop="password" label="Password")-->
-        <!--    el-input(type="password" v-model="loginForm.password" v-on:keyup.enter="submitForm('loginForm')" auto-complete="off")-->
-        <!--  el-form-item-->
-        <!--    el-button.auth-layout_submit.full-width(size="large" type="primary" @click="submitForm('loginForm')" :loading="isLoading")-->
-        <!--      | Enter-->
-        <!--  el-form-item.item-forgot-password-->
-        <!--    router-link(:to="{ name: 'forgot-password'}") Forgot password?-->
-      footer.auth-layout_footer
-        | No account yet?
-        router-link(:to="{ name: 'register'}") Register
+    div.q-pa-lg
+        q-card 
+            q-card-title(style="text-align: center;") 
+                h5 Login
+            q-card-main
+                <q-input v-model="loginForm.email" type="email" float-label="Email" />
+                <q-input v-model="loginForm.password" type="password" float-label="Password" />
+    
+                    <!--el-form.auth-layout_card-content.card-content(:model="loginForm" :rules="rules" ref="loginForm")-->
+                    <!--  el-form-item(prop="email" label="Email")-->
+                    <!--    el-input(v-model="loginForm.email" v-on:keyup.enter="submitForm('loginForm')" auto-complete="off")-->
+                    <!--  el-form-item(prop="password" label="Password")-->
+                    <!--    el-input(type="password" v-model="loginForm.password" v-on:keyup.enter="submitForm('loginForm')" auto-complete="off")-->
+                q-btn.q-pa-lg.full-width(size="medium" type="primary" @click="submitForm('loginForm')" :loading="isLoading")
+                    | Enter
+            q-card-separator
+        div.q-pa-lg(style="text-align: center;")
+            router-link(:to="{ name: 'forgot-password'}") Forgot password?
+        div.q-pa-lg(style="text-align: center;")
+            | No account yet? 
+            |&nbsp;
+            router-link(:to="{ name: 'register'}") Register
 </template>
 
 
-<script>
-  import {
-    QCard,
-    QCardTitle,
-    QCardMain,
-    QCardMedia,
-    QCardSeparator,
-    QCardActions
-   } from '/node_modules/quasar-framework/dist/quasar.mat.common.js'
+<script lang='coffee'>
+    import { QInput, QBtn, QCard, QCardTitle, QCardMain, QCardMedia, QCardSeparator, QCardActions } from '/node_modules/quasar-framework/dist/quasar.mat.common.js'
 
-  export default {
-    name: 'auth-login',
-    components: {
-      QCard,
-      QCardTitle,
-      QCardMain,
-      QCardMedia,
-      QCardSeparator,
-      QCardActions
-        },
-
-    data: () => ({
-      isLoading: false,
-      loginForm: {
-        email: '',
-        password: ''
-      },
-      rules: {
-        email: [
-          { required: true, message: 'Please insert your email!', trigger: 'blur' },
-          { type: 'email', message: 'Please insert is valid email!', trigger: 'blur,change' }
-        ],
-        password: [
-          { required: true, message: 'Please insert your password!', trigger: 'blur' },
-        ],
-      }
-    }),
-    methods: {
-      submitForm(formName) {
-        this.isLoading = true
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            const dataForm = this.loginForm
-            const email = dataForm.email,
-                  password = dataForm.password
-            Meteor.loginWithPassword(email, password, (err) => {
-  						if (err) {
-                this.$notify.error({
-                  title: 'Sorry',
-                  message: err.reason,
-                  offset: 100
-                })
-                this.isLoading = false
-  						} else {
-                this.$router.push({ name: 'admin-dashboard' })
-                this.$notify.success({
-                  title: 'Welcome',
-                  message: `Good to see you!`,
-                  offset: 100,
-                  duration: 9000
-                })
-  						}
-  					})
-          } else {
-            this.$notify.error({
-              title: 'Sorry!!!',
-              message: 'All fields are required',
-              offset: 100
-            })
-            this.isLoading = false
-          }
-        })
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields()
-      }
-    }
-  }
+    return
+        name: 'auth-login'
+        components: {
+            QInput
+            QBtn
+            QCard
+            QCardTitle
+            QCardMain
+            QCardMedia
+            QCardSeparator
+            QCardActions
+        }
+        data: -> 
+            isLoading: false
+            loginForm:
+                email: ''
+                password: ''
+            rules:
+                email: [
+                  { required: true, message: 'Please insert your email!', trigger: 'blur' }
+                  { type: 'email', message: 'Please insert is valid email!', trigger: 'blur,change' }
+                ]
+                password: [
+                  { required: true, message: 'Please insert your password!', trigger: 'blur' },
+                ]
+                
+        # methods:
+        #     submitForm: (formName)->
+        #         @isLoading = true
+        #         @$refs[formName].validate( (valid)=>
+        #             if valid
+        #                 dataForm = @loginForm
+        #                 constemail = dataForm.email
+        #                 password = dataForm.password
+        #                 Meteor.loginWithPassword(email, password, (err) =>
+        #                     if err
+        #                         @$notify.error({
+        #                             title: 'Sorry',
+        #                             message: err.reason,
+        #                             offset: 100
+        #                         })
+        #                         @isLoading = false
+        #                     else
+        #                         @$router.push({ name: 'admin-dashboard' })
+        #                         @$notify.success({
+        #                             title: 'Welcome',
+        #                             message: `Good to see you!`,
+        #                             offset: 100,
+        #                             duration: 9000
+        #                         })
+        #                 )
+        #             else
+        #                 @$notify.error({
+        #                     title: 'Sorry!!!',
+        #                     message: 'All fields are required',
+        #                     offset: 100
+        #                 })
+        #                 @isLoading = false
+        #         )
+        #     resetForm: (formName)->
+        #         @$refs[formName].resetFields()
 </script>
